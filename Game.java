b2d358069eb7d789b0fdefd28696b5e34fad0663
April Gray <aprgray5@gmail.com>
2020-11-07 14:33:52 -0600

    Add defualt time

--- Game.java
+++ Game.java
@@ -1,98 +1,104 @@
 
 
 public class Game
-{	
-	Player black;
-	Player white;
-	int blackTime;
-	int whiteTime; 
-	static int blackScore;
-	static int whiteScore;
-	static boolean isBlacksTurn = true;
-	static boolean gameOver = false;
-	static int consecutivePasses = 0;
-	
-	public Game(Player black, Player white)
-	{
-		this.black = black;
-		this.white = white;
-		this.blackTime = 0; // temp time
-		this.whiteTime = 0; // temp time
-		isBlacksTurn = true;
-		new Board(); //set up board
-		updateScores(); //setting initial scores
-		
-	}
-	
-	public static void updateScores()
-	{
-		int[] scores = Board.calculateScores();
-		blackScore = scores[0];
-		whiteScore = scores[1];
-	}
-	
-	public static void takeTurn()
-	{		
-		Game.updateScores();
-		isBlacksTurn = !isBlacksTurn;
-		Board.updateValidMoves();
-		
-		if(Game.checkGameOver())
-		{
-			Game.quitGame();
-			return;
-		}
-		
-		GameUI.refresh(GameUI.primaryStage);
-	}
-	
-	public static void passTurn()
-	{
-		if(!Board.hasValidMoves())
-		{
-			consecutivePasses++;
-			if(consecutivePasses == 2)
-			{
-				quitGame();
-			}
-			takeTurn();
-		}		
-	}
-	
-	public static void quitGame()
-	{
-		GameUI.refresh(GameUI.primaryStage);
-		Game.updateScores();
-		GameUI.showResults();		
-	}
-	
-	/*
-	 * Checks if the game is over.
-	 * The game is over when either the board is full of pieces or
-	 * when both players have no valid moves remaining (indicated by 2 consecutive passes).
-	 * 
-	 */
-	public static boolean checkGameOver()
-	{
-		return (consecutivePasses>1) || Board.isFull();
-	}
-	
-	public String toString()
-	{
-		return ""; //TBD
-	}
+{ 
+ Player black;
+ Player white;
+ static int blackTime;
+ static int whiteTime; 
+ static int blackScore;
+ static int whiteScore;
+ static boolean isBlacksTurn = true;
+ static boolean gameOver = false;
+ static int consecutivePasses = 0;
+ 
+ public Game(Player black, Player white)
+ {
+  this.black = black;
+  this.white = white;
+  isBlacksTurn = true;
+  new Board(); //set up board
+  updateScores(); //setting initial scores
+  resetTimer();
+  
+ }
+ 
+ public static void resetTimer() {
+  blackTime = 900000; // temp time
+  whiteTime = 900000; // temp time
+ }
 
-	public static void quitGameByForfeit()
-	{
-		if(isBlacksTurn)
-		{
-			blackScore = Integer.MIN_VALUE;
-		}
-		else
-		{
-			whiteScore = Integer.MIN_VALUE;
-		}
-		GameUI.showResults();
-	}
-	
+ public static void updateScores()
+ {
+  int[] scores = Board.calculateScores();
+  blackScore = scores[0];
+  whiteScore = scores[1];
+ }
+ 
+ public static void takeTurn()
+ {  
+  Game.updateScores();
+  isBlacksTurn = !isBlacksTurn;
+  Board.updateValidMoves();
+  
+  if(Game.checkGameOver())
+  {
+   Game.quitGame();
+   return;
+  }
+  
+  GameUI.refresh(GameUI.primaryStage);
+ }
+ 
+ public static void passTurn()
+ {
+  if(!Board.hasValidMoves())
+  {
+   consecutivePasses++;
+   if(consecutivePasses == 2)
+   {
+    quitGame();
+   }
+   takeTurn();
+  }  
+ }
+ 
+ public static void quitGame()
+ {
+  GameUI.refresh(GameUI.primaryStage);
+  Game.updateScores();
+  Game.resetTimer();
+  GameUI.showResults();  
+ }
+ 
+ /*
+  * Checks if the game is over.
+  * The game is over when either the board is full of pieces or
+  * when both players have no valid moves remaining (indicated by 2 consecutive passes).
+  * 
+  */
+ public static boolean checkGameOver()
+ {
+  return (consecutivePasses>1) || Board.isFull();
+ }
+ 
+ public String toString()
+ {
+  return ""; //TBD
+ }
+
+ public static void quitGameByForfeit()
+ {
+  if(isBlacksTurn)
+  {
+   blackScore = Integer.MIN_VALUE;
+  }
+  else
+  {
+   whiteScore = Integer.MIN_VALUE;
+  }
+  Game.resetTimer();
+  GameUI.showResults();
+ }
+ 
 }
--- GameUI.java
+++ GameUI.java
@@ -22,261 +22,262 @@ import javafx.stage.Stage;
 
 public class GameUI
 {
-	Game game;
-	public static Stage primaryStage;
+ Game game;
+ public static Stage primaryStage;
 
-	public static void main(String[] args)
-	{
-		Application.launch(args);
-	}
+ public static void main(String[] args)
+ {
+  Application.launch(args);
+ }
 
-	public void start(Stage primaryStage)
-	{
-		/*
-		 * Starting a new game
-		 */
-		
-		GameUI.primaryStage = primaryStage;
-		
-		game = new Game((new Player()), new Player()); // Change this once sign in is complete
+ public void start(Stage primaryStage)
+ {
+  /*
+   * Starting a new game
+   */
+  
+  GameUI.primaryStage = primaryStage;
+  
+  game = new Game((new Player()), new Player()); // Change this once sign in is complete
 
-		//GameUI.primaryStage = primaryStage;
-		
-		refresh(primaryStage);
+  //GameUI.primaryStage = primaryStage;
+  
+  refresh(primaryStage);
 
-	}
+ }
 
-	public static FlowPane showPlayers()
-	{
-		FlowPane panePlayers = new FlowPane();
-		panePlayers.setAlignment(Pos.CENTER);
-		panePlayers.setPadding(new Insets(20, 20, 20, 20));
-		panePlayers.setHgap(20);
+ public static FlowPane showPlayers()
+ {
+  FlowPane panePlayers = new FlowPane();
+  panePlayers.setAlignment(Pos.CENTER);
+  panePlayers.setPadding(new Insets(20, 20, 20, 20));
+  panePlayers.setHgap(20);
 
-		Label LbPlayer1 = new Label("Player1"); // temp name
-		Label LbPlayer2 = new Label("Player2"); // temp name
+  Label LbPlayer1 = new Label("Player1"); // temp name
+  Label LbPlayer2 = new Label("Player2"); // temp name
 
-		Label LbPlayer1Score = new Label("" + Game.blackScore); // temp var
-		Label LbPlayer2Score = new Label("" + Game.whiteScore); // temp var
+  Label LbPlayer1Score = new Label("" + Game.blackScore ); // temp var
+  Label LbPlayer2Score = new Label("" + Game.whiteScore); // temp var
 
-		if(Game.isBlacksTurn)
-		{
-			LbPlayer1.setStyle("-fx-border-color: red; -fx-border-width: 4");
-		}
-		else
-		{
-			LbPlayer2.setStyle("-fx-border-color: red; -fx-border-width: 4");
-		}
-		
-		Circle tokenBlack = new Circle();
-		tokenBlack.setRadius(15);
-		tokenBlack.setFill(Color.BLACK);
+  if(Game.isBlacksTurn)
+  {
+   LbPlayer1.setStyle("-fx-border-color: red; -fx-border-width: 4");
+  }
+  else
+  {
+   LbPlayer2.setStyle("-fx-border-color: red; -fx-border-width: 4");
+  }
+  
+  Circle tokenBlack = new Circle();
+  tokenBlack.setRadius(15);
+  tokenBlack.setFill(Color.BLACK);
 
-		Circle tokenWhite = new Circle();
-		tokenWhite.setRadius(15);
-		tokenWhite.setStroke(Color.BLACK);
-		tokenWhite.setFill(Color.WHITE);
+  Circle tokenWhite = new Circle();
+  tokenWhite.setRadius(15);
+  tokenWhite.setStroke(Color.BLACK);
+  tokenWhite.setFill(Color.WHITE);
 
-		// This token is for spacing purposes
-		Circle tokenInvis1 = new Circle();
-		tokenInvis1.setRadius(15);
-		tokenInvis1.setStroke(Color.TRANSPARENT);
-		tokenInvis1.setFill(Color.TRANSPARENT);
+  // This token is for spacing purposes
+  Circle tokenInvis1 = new Circle();
+  tokenInvis1.setRadius(15);
+  tokenInvis1.setStroke(Color.TRANSPARENT);
+  tokenInvis1.setFill(Color.TRANSPARENT);
 
-		// This token is for spacing purposes
-		Circle tokenInvis2 = new Circle();
-		tokenInvis2.setRadius(15);
-		tokenInvis2.setStroke(Color.TRANSPARENT);
-		tokenInvis2.setFill(Color.TRANSPARENT);
+  // This token is for spacing purposes
+  Circle tokenInvis2 = new Circle();
+  tokenInvis2.setRadius(15);
+  tokenInvis2.setStroke(Color.TRANSPARENT);
+  tokenInvis2.setFill(Color.TRANSPARENT);
 
-		// This token is for spacing purposes
-		Circle tokenInvis3 = new Circle();
-		tokenInvis3.setRadius(15);
-		tokenInvis3.setStroke(Color.TRANSPARENT);
-		tokenInvis3.setFill(Color.TRANSPARENT);
+  // This token is for spacing purposes
+  Circle tokenInvis3 = new Circle();
+  tokenInvis3.setRadius(15);
+  tokenInvis3.setStroke(Color.TRANSPARENT);
+  tokenInvis3.setFill(Color.TRANSPARENT);
 
-		LbPlayer1.setFont(new Font(20));
-		LbPlayer2.setFont(new Font(20));
-		LbPlayer1Score.setFont(new Font(20));
-		LbPlayer2Score.setFont(new Font(20));
+  LbPlayer1.setFont(new Font(20));
+  LbPlayer2.setFont(new Font(20));
+  LbPlayer1Score.setFont(new Font(20));
+  LbPlayer2Score.setFont(new Font(20));
 
-		panePlayers.getChildren().addAll(LbPlayer1, tokenInvis1, LbPlayer1Score, tokenBlack, tokenInvis2, tokenWhite,
-				LbPlayer2Score, tokenInvis3, LbPlayer2);
+  panePlayers.getChildren().addAll(LbPlayer1, tokenInvis1, LbPlayer1Score, tokenBlack, tokenInvis2, tokenWhite,
+    LbPlayer2Score, tokenInvis3, LbPlayer2);
 
-		return panePlayers;
-	}
-	
-	private static FlowPane showTimers() {
-		FlowPane paneTimer = new FlowPane();
-		paneTimer.setAlignment(Pos.CENTER);
-		paneTimer.setPadding(new Insets(20, 20, 20, 20));
-		paneTimer.setHgap(20);
+  return panePlayers;
+ }
+ 
+ private static FlowPane showTimers() {
+  FlowPane paneTimer = new FlowPane();
+  paneTimer.setAlignment(Pos.CENTER);
+  paneTimer.setPadding(new Insets(20, 20, 20, 20));
+  paneTimer.setHgap(20);
 
-		Label LbPlayer1 = new Label("Player1 Time"); // temp name
-		Label LbPlayer2 = new Label("Player2 Time"); // temp name
+  Label LbPlayer1 = new Label("Player1 Time"); // temp name
+  Label LbPlayer2 = new Label("Player2 Time"); // temp name
 
-		Label LbPlayer1Score = new Label("" + Game.blackScore); // temp var
-		Label LbPlayer2Score = new Label("" + Game.whiteScore); // temp var
-		
-		paneTimer.getChildren().addAll(LbPlayer1, LbPlayer1Score, LbPlayer2, LbPlayer2Score);
-		
-		
-		return paneTimer;
-	}
+  
+  Label LbPlayer1Score = new Label("" + (Game.blackTime/(1000*60))%60 + ":" + (Game.blackTime/1000)%60); // temp var
+  Label LbPlayer2Score = new Label("" + (Game.whiteTime/(1000*60))%60 + ":" + (Game.whiteTime/1000)%60); // temp var
+  
+  paneTimer.getChildren().addAll(LbPlayer1, LbPlayer1Score, LbPlayer2, LbPlayer2Score);
+  
+  
+  return paneTimer;
+ }
 
-	public static GridPane showBoard()
-	{
-		GridPane paneBoard = new GridPane();
-		paneBoard.setAlignment(Pos.CENTER);
-		paneBoard.setGridLinesVisible(true);
+ public static GridPane showBoard()
+ {
+  GridPane paneBoard = new GridPane();
+  paneBoard.setAlignment(Pos.CENTER);
+  paneBoard.setGridLinesVisible(true);
 
-		for (int i = 0; i < 8; i++)
-		{
-			for (int j = 0; j < 8; j++)
-			{
-				paneBoard.add(Board.board[i][j], j, i);
+  for (int i = 0; i < 8; i++)
+  {
+   for (int j = 0; j < 8; j++)
+   {
+    paneBoard.add(Board.board[i][j], j, i);
 
-				if (Board.board[i][j].getToken() == 'B')
-				{
-					Circle tokenBlack = new Circle();
-					tokenBlack.setRadius(25);
-					tokenBlack.setStroke(Color.BLACK);
-					tokenBlack.setFill(Color.BLACK);
-					paneBoard.add(tokenBlack, j, i);
-				} else if (Board.board[i][j].getToken() == 'W')
-				{
-					Circle tokenWhite = new Circle();
-					tokenWhite.setRadius(25);
-					tokenWhite.setStroke(Color.BLACK);
-					tokenWhite.setFill(Color.WHITE);
-					paneBoard.add(tokenWhite, j, i);
-				} else if (Board.board[i][j].getToken() == ' ')
-				{
-					Rectangle tokenBlank = new Rectangle();
-					tokenBlank.setWidth(50);
-					tokenBlank.setHeight(50);
-					tokenBlank.setStroke(Color.BLACK);
-					tokenBlank.setFill(Color.TRANSPARENT);
-					paneBoard.add(tokenBlank, j, i);
-				}
-				
-			}
-		}
+    if (Board.board[i][j].getToken() == 'B')
+    {
+     Circle tokenBlack = new Circle();
+     tokenBlack.setRadius(25);
+     tokenBlack.setStroke(Color.BLACK);
+     tokenBlack.setFill(Color.BLACK);
+     paneBoard.add(tokenBlack, j, i);
+    } else if (Board.board[i][j].getToken() == 'W')
+    {
+     Circle tokenWhite = new Circle();
+     tokenWhite.setRadius(25);
+     tokenWhite.setStroke(Color.BLACK);
+     tokenWhite.setFill(Color.WHITE);
+     paneBoard.add(tokenWhite, j, i);
+    } else if (Board.board[i][j].getToken() == ' ')
+    {
+     Rectangle tokenBlank = new Rectangle();
+     tokenBlank.setWidth(50);
+     tokenBlank.setHeight(50);
+     tokenBlank.setStroke(Color.BLACK);
+     tokenBlank.setFill(Color.TRANSPARENT);
+     paneBoard.add(tokenBlank, j, i);
+    }
+    
+   }
+  }
 
-		return paneBoard;
-	}
+  return paneBoard;
+ }
 
-	/*
-	 * This method refreshes the display of the board
-	 */
-	public static void refresh(Stage primaryStage)
-	{
-		/*
-		 * Creating the buttons pane
-		 */
-		
-		// Buttons
-		Button btPass = new Button("Pass Turn");
-		Button btQuit = new Button("Quit Game");
+ /*
+  * This method refreshes the display of the board
+  */
+ public static void refresh(Stage primaryStage)
+ {
+  /*
+   * Creating the buttons pane
+   */
+  
+  // Buttons
+  Button btPass = new Button("Pass Turn");
+  Button btQuit = new Button("Quit Game");
 
-		// Setting actions for buttons
-		btQuit.setOnAction(e -> Game.quitGameByForfeit());
-		btPass.setOnAction(e -> Game.passTurn());
+  // Setting actions for buttons
+  btQuit.setOnAction(e -> Game.quitGameByForfeit());
+  btPass.setOnAction(e -> Game.passTurn());
 
-		HBox paneButton = new HBox(100);
-		paneButton.setPadding(new Insets(15, 15, 15, 15));
-		paneButton.setAlignment(Pos.CENTER);
-		paneButton.getChildren().addAll(btPass, btQuit);
+  HBox paneButton = new HBox(100);
+  paneButton.setPadding(new Insets(15, 15, 15, 15));
+  paneButton.setAlignment(Pos.CENTER);
+  paneButton.getChildren().addAll(btPass, btQuit);
 
-		GridPane paneBoard = showBoard();
+  GridPane paneBoard = showBoard();
 
-		FlowPane panePlayers = showPlayers();
-		
-		FlowPane paneTimer = showTimers();
-		
-		VBox paneTop = new VBox(5); // the amount of vertical space between each child
-		paneTop.setAlignment(Pos.CENTER);
-		paneTop.getChildren().addAll(paneButton, paneTimer);
+  FlowPane panePlayers = showPlayers();
+  
+  FlowPane paneTimer = showTimers();
+  
+  VBox paneTop = new VBox(5); // the amount of vertical space between each child
+  paneTop.setAlignment(Pos.CENTER);
+  paneTop.getChildren().addAll(paneButton, paneTimer);
 
 
-		/*
-		 * Combining all panes to a single BorderPane
-		 */
+  /*
+   * Combining all panes to a single BorderPane
+   */
 
-		BorderPane paneGame = new BorderPane();
-		paneGame.setTop(paneTop);
-		paneGame.setCenter(paneBoard);
-		paneGame.setBottom(panePlayers);
-		// timers at top
+  BorderPane paneGame = new BorderPane();
+  paneGame.setTop(paneTop);
+  paneGame.setCenter(paneBoard);
+  paneGame.setBottom(panePlayers);
+  // timers at top
 
-		Scene sceneGame = new Scene(paneGame, 600, 600);
+  Scene sceneGame = new Scene(paneGame, 600, 600);
 
-		primaryStage.setTitle("Reversi"); // Set the stage title
-		primaryStage.setScene(sceneGame); // Place the scene in the stage
-		primaryStage.show(); // Display the stage
-		
+  primaryStage.setTitle("Reversi"); // Set the stage title
+  primaryStage.setScene(sceneGame); // Place the scene in the stage
+  primaryStage.show(); // Display the stage
+  
 
-	}
+ }
 
-	
-	public static void showResults()
-	{
-		BorderPane paneResults = new BorderPane();
-		Button btBackToMain = new Button("Back To Main Screen");
-		btBackToMain.setOnAction(e -> (new ReversiApp()).start(primaryStage));
-		
-		if(Game.blackScore>Game.whiteScore)
-		{
-			Alert a1 = new Alert(AlertType.NONE,"BLACK HAS WON !", ButtonType.OK);
-			Optional<ButtonType> result = a1.showAndWait();
-			if (result.get() == ButtonType.OK)
-			{
-				beginningStage(primaryStage);
-			}
-		}
-		else if(Game.whiteScore>Game.blackScore)
-		{
-			Alert a1 = new Alert(AlertType.NONE,"WHITE HAS WON !", ButtonType.OK); 
-			Optional<ButtonType> result = a1.showAndWait();
-			if (result.get() == ButtonType.OK)
-			{
-				beginningStage(primaryStage);
-			}
-		}
-		else if(Game.blackScore==Game.whiteScore)
-		{
-			Alert a1 = new Alert(AlertType.NONE,"IT'S A TIE !", ButtonType.OK); 
-			Optional<ButtonType> result = a1.showAndWait();
-			if (result.get() == ButtonType.OK)
-			{
-				beginningStage(primaryStage);
-			}
-		}
-	}
-	
-	
-	public static void beginningStage(Stage primaryStage)
-	{
-		Button btRegister = new Button("Sign In");
-		Button btStats = new Button("View Statistics");
-		Button btConfig = new Button("Configure Game");
-		Button btGame = new Button("Start Game");
+ 
+ public static void showResults()
+ {
+  BorderPane paneResults = new BorderPane();
+  Button btBackToMain = new Button("Back To Main Screen");
+  btBackToMain.setOnAction(e -> (new ReversiApp()).start(primaryStage));
+  
+  if(Game.blackScore>Game.whiteScore)
+  {
+   Alert a1 = new Alert(AlertType.NONE,"BLACK HAS WON !", ButtonType.OK);
+   Optional<ButtonType> result = a1.showAndWait();
+   if (result.get() == ButtonType.OK)
+   {
+    beginningStage(primaryStage);
+   }
+  }
+  else if(Game.whiteScore>Game.blackScore)
+  {
+   Alert a1 = new Alert(AlertType.NONE,"WHITE HAS WON !", ButtonType.OK); 
+   Optional<ButtonType> result = a1.showAndWait();
+   if (result.get() == ButtonType.OK)
+   {
+    beginningStage(primaryStage);
+   }
+  }
+  else if(Game.blackScore==Game.whiteScore)
+  {
+   Alert a1 = new Alert(AlertType.NONE,"IT'S A TIE !", ButtonType.OK); 
+   Optional<ButtonType> result = a1.showAndWait();
+   if (result.get() == ButtonType.OK)
+   {
+    beginningStage(primaryStage);
+   }
+  }
+ }
+ 
+ 
+ public static void beginningStage(Stage primaryStage)
+ {
+  Button btRegister = new Button("Sign In");
+  Button btStats = new Button("View Statistics");
+  Button btConfig = new Button("Configure Game");
+  Button btGame = new Button("Start Game");
 
-		// Setting actions for buttons
-		btRegister.setOnAction(e -> (new RegistrationUI()).start(primaryStage));
-		btStats.setOnAction(e -> (new StatisticsUI()).start(primaryStage));
-		btConfig.setOnAction(e -> (new ConfigureUI()).start(primaryStage));
-		btGame.setOnAction(e -> (new GameUI()).start(primaryStage));
+  // Setting actions for buttons
+  btRegister.setOnAction(e -> (new RegistrationUI()).start(primaryStage));
+  btStats.setOnAction(e -> (new StatisticsUI()).start(primaryStage));
+  btConfig.setOnAction(e -> (new ConfigureUI()).start(primaryStage));
+  btGame.setOnAction(e -> (new GameUI()).start(primaryStage));
 
-		VBox paneInitial = new VBox(25); // the amount of vertical space between each child
-		paneInitial.setAlignment(Pos.CENTER);
-		paneInitial.getChildren().addAll(btRegister, btStats, btConfig, btGame);
+  VBox paneInitial = new VBox(25); // the amount of vertical space between each child
+  paneInitial.setAlignment(Pos.CENTER);
+  paneInitial.getChildren().addAll(btRegister, btStats, btConfig, btGame);
 
-		Scene sceneInitial = new Scene(paneInitial, 500, 500);
+  Scene sceneInitial = new Scene(paneInitial, 500, 500);
 
-		primaryStage.setTitle("Reversi"); // Set the stage title
-		primaryStage.setScene(sceneInitial); // Place the scene in the stage
-		primaryStage.show(); // Display the stage
+  primaryStage.setTitle("Reversi"); // Set the stage title
+  primaryStage.setScene(sceneInitial); // Place the scene in the stage
+  primaryStage.show(); // Display the stage
 
-	}
+ }
 }
