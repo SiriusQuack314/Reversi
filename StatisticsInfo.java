885a256ba2439f8f2b2137cb8f580eb24a337dca
BrentfordH <71101446+BrentfordH@users.noreply.github.com>
2020-11-11 16:58:00 -0600

    Update StatisticsInfo.java

--- StatisticsInfo.java
+++ StatisticsInfo.java
@@ -1,212 +1,212 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Scanner;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.stage.Window;
-
-public class SigninUI
+public class StatisticsInfo
 {
-	public static void main(String[] args)
-	{
-		Application.launch(args);
-	}
-	
-		static File newFile = new File("reginfo.txt");
-		int count = 0;
-
-	//Creates or detects file used to store registration info.
+  
+	//Creates the stats.txt file to save player's data
 	public static void createData()
-	   {
-		   try 
-		    {  
-		      if (newFile.createNewFile()) 
-		      {
-		        System.out.println("File created: " + newFile.getName());
-		      } 
-		      
-		      else 
-		      {
-		        System.out.println("Please enter username and password");
-		      }
-		    } 
-		    
-		    
-		    catch (IOException e) 
-		    {
-		      System.out.println("An error occurred.");
-		      e.printStackTrace();
-		    }
-		   
-	   }
-	
-	//Used to test if the file was being written correctly.
-	//Can be removed on final iteration.
-	public static void readFile() {
-		try {
-		      File myObj = new File("reginfo.txt");
-		      Scanner myReader = new Scanner(myObj);
-		      while (myReader.hasNextLine()) {
-		        String data = myReader.nextLine();
-		        System.out.println(data);
-		      }
-		      myReader.close();
-		    } catch (FileNotFoundException e) {
-		      System.out.println("An error occurred.");
-		      e.printStackTrace();
-		    }
-	}
-	
-	//Reads the first line of the file and returns the username.
-	public static String userRead() {
-		int n = 0; // The line number
-		String line = null;
-	      try{
-	        line = Files.readAllLines(Paths.get("reginfo.txt")).get(n);
+    {
+	   try 
+	    {
+	      File newFile = new File("stats.txt");
+	      if (newFile.createNewFile()) 
+	      {
+	        System.out.println("File created: " + newFile.getName());
 	      } 
-	      catch(IOException e){
-	        System.out.println(e);
-	      }
-		return line;
-	}
-	
-	//Reads the second line of the file and returns the password.
-	public static String passRead() {
-		int n = 1; // The line number
-		String line = null;
-	      try{
-	        line = Files.readAllLines(Paths.get("reginfo.txt")).get(n);
-	      } 
-	      catch(IOException e){
-	        System.out.println(e);
-	      }
-	      return line;
 	      
-	}
+	      else 
+	      {
+	        System.out.println("User Stats Found");
+	      }
+	    } 
+	    
+	    
+	    catch (IOException e) 
+	    {
+	      System.out.println("An error occurred.");
+	      e.printStackTrace();
+	    }
+	   
+    }
 	
-	public void start(Stage primaryStage)
+	//Writes a new player to the file
+	public static void write(String p) throws IOException
 	{
-		/*
-		 * Sign-in Scene
-		 * 
-		 * Here a user can sign-in as an existing player. To sign-in, one enters their registered
-		 * name of five letters and digits; and their password of five digits.
-		 * 
-		 */
-		createData();
+		Scanner fs = new Scanner(new File("stats.txt"));
+		FileWriter fw = new FileWriter("stats.txt", true);
 		
-		// Labels & Text fields
-		Label LbUsername = new Label("Username"); // Labels will be on left..
-		TextField TxUsername = new TextField();
-		TxUsername.setMaxWidth(250);
-
-		Label LbPassword = new Label("Password");
-		TextField TxPassword = new TextField();
-		TxPassword.setMaxWidth(250);
-
-		// Buttons
-		Button btLogin = new Button("Login");
-		Button btRegister = new Button("Register");
-		Button btBack = new Button("Back");
-
-		// Setting actions for buttons
-		btBack.setOnAction(e -> (new ReversiApp()).start(primaryStage));
-		btRegister.setOnAction(e -> (new RegisterUI()).start(primaryStage));
-		btLogin.setOnAction(new EventHandler<ActionEvent>() {
-			//Handles the login button press based on login info input.
-			@Override
-		    public void handle(ActionEvent event) {
-				try {
-				if(TxUsername.getText().isEmpty()) {
-		            showAlert(Alert.AlertType.ERROR, primaryStage.getScene().getWindow(), 
-		            "Form Error", "Please enter a Username.");
-		            readFile();
-		            return;
-		        }
-		        if(TxPassword.getText().isEmpty()) {
-		            showAlert(Alert.AlertType.ERROR, primaryStage.getScene().getWindow(), 
-		            "Form Error", "Please enter a Password.");
-		            return;
-		        }        	
-		        if(!TxUsername.getText().equals(PlayerInfo.userCheck(TxUsername.getText())) || TxPassword.getText().equals(PlayerInfo.passCheck(TxUsername.getText()))){
-		        	showAlert(Alert.AlertType.ERROR, primaryStage.getScene().getWindow(), 
-				            "Form Error", "Username or Password is incorrect.");
-		        	return;
-		        }
-		        
-				if(count!=2)	
-		        if(TxUsername.getText().equals(PlayerInfo.userCheck(TxUsername.getText())) && TxPassword.getText().equals(PlayerInfo.passCheck(TxUsername.getText())))
-					{
-						showAlert(Alert.AlertType.CONFIRMATION, primaryStage.getScene().getWindow(), 
-						        "Login Successful!", "Welcome " + TxUsername.getText());
-						
-						//This lil bit sets the player's username to his login, marks that he is logged in, and randomly assigns him white or black
-						
-						if(count==0)
-						{
-							Player.setUsername(TxUsername.getText());
-							Player.setLogin();
-							Player.setPriority();
-							count++;
-							return;
-						}
-						
-						else if(count==1)
-						{
-							Player2.setUsername(TxUsername.getText());
-							Player2.setLogin();
-							count++;
-							return;
-						}
-					}
-				
-		        else if(count==2)
-				{
-					showAlert(Alert.AlertType.ERROR,primaryStage.getScene().getWindow(),"Error","There are already 2 players signed in");
-					return;
-				}
-				
-				} catch (FileNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-		    }
-		});
-
-		
-		
-		VBox paneRegister = new VBox(25); // the amount of vertical space between each child
-		paneRegister.setAlignment(Pos.CENTER);
-		paneRegister.getChildren().addAll(LbUsername, TxUsername, LbPassword, TxPassword, btLogin, btRegister, btBack);
-
-		Scene sceneRegister = new Scene(paneRegister, 500, 500);
-		
-		primaryStage.setTitle("Reversi"); // Set the stage title
-		primaryStage.setScene(sceneRegister); // Place the scene in the stage
-		primaryStage.show(); // Display the stage
-	}
-	//Alert method that allows for alert messages to appear
-	private void showAlert(AlertType alertType, Window owner, String title, String message) {
-			Alert alert = new Alert(alertType);
-			alert.setTitle(title);
-			alert.setHeaderText(null);
-			alert.setContentText(message);
-			alert.initOwner(owner);
-			alert.show();
+		while(fs.hasNextLine())
+		{
+			fs.nextLine();
 			
 		}
+		fw.write(p+" 0 0\n");
+		fw.close();
+	}
+	
+	
+	//This finds a player's name, wins, and losses, and returns them. I may delete this.
+	//EDIT: This now finds a player and returns true if found. False if not.
+	public static boolean find(String p) throws IOException
+	{
+		Scanner fs = new Scanner(new File("stats.txt"));
+		String pn = p;
+		int pw = 0;
+		int pl = 0;
+		String check = "";
+		String [] check1 = new String[3];
+		
+		while(fs.hasNextLine())
+		{
+			
+			check=fs.nextLine();
+			check1 = check.split(" ");
+			if(check1[0].equals(pn))
+			{
+				pw=Integer.parseInt(check1[1]);
+				pl=Integer.parseInt(check1[2]);
+				return true;
+			}
+			
+		}
+		
+		//return pn+" has "+pw +" wins and "+pl+" losses";
+		return false;
+	}
+	
+	
+	//Overwrites a players previous data
+	public static void overWrite(String p, int w, int l) throws IOException
+	{
+		Scanner fs = new Scanner(new File("stats.txt"));
+		
+		String pn=p;
+		String old="";
+		String check = "";
+		String [] check1 = new String[3];
+		
+		while(fs.hasNextLine())
+		{
+			
+			check=fs.nextLine();
+			check1 = check.split(" ");
+			if(check1[0].equals(pn))
+			{
+				//This essentially deletes the player's old data, so it can be rewritten later
+			}
+			else
+			{
+				old+=check+"\n";			
+			}
+		}
+		
+		FileWriter fw = new FileWriter("stats.txt");
+		fw.write(old);
+		fw.append(p+" "+w+" "+l+"\n");
+		fw.close();
+	}
+	
+	//Deletes a Player
+	public static void delete(String p) throws IOException
+	{
+		Scanner fs = new Scanner(new File("stats.txt"));
+		
+			
+		String pn=p;
+		//String old = fs.nextLine();
+		String old="";
+		String check = "";
+		String [] check1 = new String[3];
+		while(fs.hasNextLine())
+		{
+				
+			check=fs.nextLine();
+			check1 = check.split(" ");
+			if(check1[0].equals(pn))
+			{
+				
+			}
+			else
+			{
+				old+=check+"\n";		
+			}
+			
+				
+		}
+			
+		FileWriter fw = new FileWriter("stats.txt");
+		fw.write(old);
+		fw.close();
+	}
+	
+	//Displays all players in the file. This may move or change
+	//EDIT: This has changed from void to String
+	public static String displayAll() throws FileNotFoundException
+	{
+		Scanner fs = new Scanner(new File("stats.txt"));
+		String check = "";
+		
+		while(fs.hasNextLine())
+		{
+			check+=fs.nextLine()+"\n";
+		}
+		
+		return check;
+	}
+	
+	//Gets player wins
+	public static int getWins(String p) throws FileNotFoundException
+	{
+		Scanner fs = new Scanner(new File("stats.txt"));
+		String pn = p;
+		int pw = 0;
+		String check = "";
+		String [] check1 = new String[3];
+		
+		while(fs.hasNextLine())
+		{
+			
+			check=fs.nextLine();
+			check1 = check.split(" ");
+			if(check1[0].equals(pn))
+			{
+				pw=Integer.parseInt(check1[1]);
+				break;
+			}
+			
+		}
+		
+		return pw;
+	}
+	
+	//Gets Player losses
+	public static int getLosses(String p) throws FileNotFoundException
+	{
+		Scanner fs = new Scanner(new File("stats.txt"));
+		String pn = p;
+		int pl = 0;
+		String check = "";
+		String [] check1 = new String[3];
+		
+		while(fs.hasNextLine())
+		{
+			
+			check=fs.nextLine();
+			check1 = check.split(" ");
+			if(check1[0].equals(pn))
+			{
+				pl=Integer.parseInt(check1[2]);
+				break;
+			}
+			
+		}
+		
+		return pl;
+	}
+
+	
+   
 }
