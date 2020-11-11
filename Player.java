8a366c8e27f627e82f0d0b476deb5d5bc848a16d
BrentfordH <71101446+BrentfordH@users.noreply.github.com>
2020-11-11 03:49:26 -0600

    Update Player.java
    Added a few functions:
    Player Priority (Whether or not they are black/will go first)
    A way to detect if they're actually logged in
    Various methods that helped make stats work

--- Player.java
+++ Player.java
@@ -1,16 +1,35 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Player
 {
-	private String username;
+	private static String username;
 	private String password;
 	private boolean isAdmin;
+	private int wins;
+	private int losses;
+	static boolean isBlack;
+	static boolean isLoggedIn=false;
 	
 	public Player()
 	{
-		
+		//place holder for now, will decide where to put this later
+		//StatisticsInfo.write(username);
 	}
 	
-	public String getUsername()
+	public Player(String pn)
+	{
+		this.username=pn;
+	}
+	
+	public static void setUsername(String pn)
+	{
+		username=pn;
+	}
+	
+	public static String getUsername()
 	{
 		return username;
 	}
@@ -31,9 +50,37 @@ public class Player
 		//TO DO
 	}
 	
+	public static void setPriority()
+	{
+		int pr = (int)(Math.random()*2);
+			if(pr==0)
+			{
+				isBlack=true;
+				Player2.isBlack=false;
+			}
+			else
+			{
+				isBlack=false;
+				Player2.isBlack=true;
+			}
+	}
+	
+	public static boolean getPriority()
+	{
+		return isBlack;
+	}
+	
+	public static void setLogin()
+	{
+		isLoggedIn=true;
+	}
+	
+	public static boolean getLogin()
+	{
+		return isLoggedIn;
+	}
+	
 	public String toString()
 	{
 		return "Username: " + username + " | Password: " + password + " | Admin: " + isAdmin;
 	}
-	
-}
