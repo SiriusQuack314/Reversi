23ab5135fc94cc55ccdbe2e72fa423006e13edaa
BrentfordH <71101446+BrentfordH@users.noreply.github.com>
2020-11-11 04:00:56 -0600

    Create Player2.java
    This is a clone of Player1 that will be needed in order to create2 player sign in possible

---
+++ Player2.java
@@ -0,0 +1,82 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Player2
+{
+	private static String username;
+	private String password;
+	private boolean isAdmin;
+	private int wins;
+	private int losses;
+	static boolean isBlack;
+	static boolean isLoggedIn=false;
+	
+	public Player2()
+	{
+		//place holder for now, will decide where to put this later
+		//StatisticsInfo.write(username);
+	}
+	
+	public Player2(String pn)
+	{
+		this.username=pn;
+	}
+	
+	public static void setUsername(String pn)
+	{
+		username=pn;
+	}
+	
+	public static String getUsername()
+	{
+		return username;
+	}
+	
+	public String getPassword()
+	{
+		return password;
+	}
+	
+	public void signIn(String username, String password)
+	{
+		//TO DO
+		//include way to recognize if they are an admin, and set isAdmin to true
+	}
+	
+	public void register(String username, String password)
+	{
+		//TO DO
+	}
+	
+	public static void setPriority()
+	{
+		int pr = (int)(Math.random()*2);
+			if(pr==0)
+				isBlack=true;
+			else
+				isBlack=false;
+	}
+	
+	public static boolean getPriority()
+	{
+		return isBlack;
+	}
+	
+	public static void setLogin()
+	{
+		isLoggedIn=true;
+	}
+	
+	public static boolean getLogin()
+	{
+		return isLoggedIn;
+	}
+	
+	public String toString()
+	{
+		return "Username: " + username + " | Password: " + password + " | Admin: " + isAdmin;
+	}
+	
+}
